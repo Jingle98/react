fbb6573df83a4ce5f6302674abfad48648ffb223
Jingle98 <sunhikingjing@gmail.com>
2022-03-11 15:58:22 +0800

    准备开始新课程

---
+++ oldFiles/src/App.js
@@ -0,0 +1,27 @@
+// 这个就是log的图片，图片也可作为一个模块使用
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+// 利用这句话将组件暴露出去，默认暴露
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,21 +1,2 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-//引入了App组件
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-
-ReactDOM.render(
-  //    <React.StrictMode> 用来检查App组件以及子组件中的写法是否合理
-  //  把代码包裹在这个里面就比较好
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-// 这个的作用是记录性能的，性能检测
-reportWebVitals();
+// 入口文件
+console.log('@')
