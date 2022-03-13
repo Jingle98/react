8ee0a868ac3093fe39711d9ad061bfb3d906158b
Jingle98 <sunhikingjing@gmail.com>
2022-03-13 21:42:49 +0800

    将第二节课的内容打包，准备开始第三节课

--- src/components/Welcome/index.js
+++
@@ -1,9 +0,0 @@
-import React,{Component} from "react";
-import './index.css'
-// 创建hello组件
-export default class Index extends Component{
-    render() {
-        // 这个样式的名字不能和hello中的title一样
-        return <h2 className={"welcome"}>Welcome!</h2>
-    }
-}
--- src/index.js
+++
@@ -1,9 +0,0 @@
-// 入口文件
-//引入核心库
-import React from 'react'
-import ReactDOM from 'react-dom'
-//引入组件
-import App from './App'
-
-// 将App渲染到页面
-ReactDOM.render(<App/>, document.getElementById("root"))
