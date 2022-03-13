8ee988d2d8f1ceeb9a2daa7526082ec5bb15bdbe
Jingle98 <sunhikingjing@gmail.com>
2022-03-13 21:40:57 +0800

    将第二节课的内容打包

---
+++ 02_src_hello_reat/components/Hello/index.js
@@ -0,0 +1,11 @@
+import React,{Component} from "react";
+//  引入样式:脚手架中引入样式的方式import
+import './index.css'
+// 创建hello组件
+
+export default class Index extends Component{
+    render() {
+        // title一会儿设置样式的时候使用
+        return <h2 className={"title"}>Hello，ll!</h2>
+    }
+}
