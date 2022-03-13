4a83404eeffedefc8b12a87af075a918028d65a1
Jingle98 <sunhikingjing@gmail.com>
2022-03-13 21:14:15 +0800

    一般会将组件的名字、样式改成index

--- src/components/Hello/index.js
+++ src/components/Hello/index.js
@@ -1,9 +1,9 @@
 import React,{Component} from "react";
 //  引入样式:脚手架中引入样式的方式import
-import './Hello.css'
+import './index.css'
 // 创建hello组件
 
-export default class Hello extends Component{
+export default class Index extends Component{
     render() {
         // title一会儿设置样式的时候使用
         return <h2 className={"title"}>Hello，ll!</h2>
--- src/components/Welcome/index.js
+++ src/components/Welcome/index.js
@@ -1,7 +1,7 @@
 import React,{Component} from "react";
-import './Welcome.css'
+import './index.css'
 // 创建hello组件
-export default class Welcome extends Component{
+export default class Index extends Component{
     render() {
         // 这个样式的名字不能和hello中的title一样
         return <h2 className={"welcome"}>Welcome!</h2>
