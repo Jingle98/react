d00a70801e507948b1072e660b1b6e2f3e5cff60
Jingle98 <sunhikingjing@gmail.com>
2022-03-13 09:10:05 +0800

    hello第一版

--- src/App.js
+++ src/App.js
@@ -1,1 +1,17 @@
 // 组件App
+// 组件有两种：类式、函数式
+//创建外壳组件APP；这里面包括所有的组件
+import React from 'react'
+
+class App extends React.Component{
+    render() {
+        return(
+            <div>
+                hello,
+            </div>
+        )
+    }
+}
+
+// 默认暴露
+export default App
--- src/index.js
+++ src/index.js
@@ -1,2 +1,9 @@
 // 入口文件
-console.log('@')
+//引入核心库
+import React from 'react'
+import ReactDOM from 'react-dom'
+//引入组件
+import App from './App'
+
+// 将App渲染到页面
+ReactDOM.render(<App/>, document.getElementById("root"))
