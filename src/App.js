cf3400b9e88aa8f46d9253f55419fdc478376230
Jingle98 <sunhikingjing@gmail.com>
2022-03-13 09:24:47 +0800

    hello写成组件的形式

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@
 // 组件有两种：类式、函数式
 //创建外壳组件APP；这里面包括所有的组件
 import React from 'react'
+import Hello from "./Hello";
 
 // 创建并暴露
 export  default class App extends React.Component{
     render() {
         return(
             <div>
-               hello, {/* 一般不建议这样写，建议写成组件的形式 */}
+                <Hello/>  {/* 组件引用的形式 */}
             </div>
         )
     }
