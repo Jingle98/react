f44ca6800fd144de3aefaf0a66d8cb66885f0223
Jingle98 <sunhikingjing@gmail.com>
2022-03-13 09:39:50 +0800

    hello组件进行管理，放到components里面

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@
 // 组件有两种：类式、函数式
 //创建外壳组件APP；这里面包括所有的组件
 import React from 'react'
-import Hello from "./Hello";
+import Hello from "./components/Hello";
 
 // 创建并暴露
 export  default class App extends React.Component{
