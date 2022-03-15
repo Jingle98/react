955c9be989638a908fb5ccd1911c74db3ffcb582
Jingle98 <sunhikingjing@gmail.com>
2022-03-15 15:48:41 +0800

    静态组件

--- src/App.jsx
+++ src/App.jsx
@@ -6,13 +6,24 @@ import List from "./components/List";
 import Footer from "./components/Footer";
 
 class App extends Component {
+    // 在render前面初始化状态
+    state = {todos:[
+            {id:'001',name:'事件1',done:'true'},
+            {id:'002',name:'事件2',done:'true'},
+            {id:'003',name:'事件3',done:'false'},
+            {id:'004',name:'事件4',done:'true'},
+        ]
+    }
     render() {
+        // 把状态取出来
+        const {todos}=this.state
         return (
             <div>
                 <div className="todo-container">
                     <div className="todo-wrap">
                         <Header/>
-                        <List/>
+                        {/*把拿到的状态传递给List*/}
+                        <List todos={todos}/>
                         <Footer/>
                     </div>
                 </div>
